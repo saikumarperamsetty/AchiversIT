9bfd6d9222ac06414f82cb177882ef5195e854c4
saikumarperamsetty <saikumar.peramsetty77@gmail.com>
2024-08-26 19:12:26 +0530

    carousel completed

--- React/ecommerce/src/components/Home/Home.jsx
+++ React/ecommerce/src/components/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Header from '../Header/Header'
+import Carousel from './Carousel'
 
 const Home = () => {
   return (
     <div>
       <Header/>
+      <Carousel/>
     </div>
   )
 }
