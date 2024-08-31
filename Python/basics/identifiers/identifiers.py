744fea6530223a5b5c70f655a88457dabd5af9ac
saikumarperamsetty <saikumar.peramsetty77@gmail.com>
2024-08-31 16:09:50 +0530

    identifiers completed

--- Python/basics/identifiers/identifiers.py
+++ Python/basics/identifiers/identifiers.py
@@ -1,31 +1,67 @@
 # a-z, A-Z, _ these only are allowed in Python
-name = 'PERAMSETTY SAI KUMAR' #valid
-MyCourse = 'Python Full Stack' #valid
-print(MyCourse)
-print(name)
+# name = 'PERAMSETTY SAI KUMAR' #valid
+# MyCourse = 'Python Full Stack' #valid
+# print(MyCourse)
+# print(name)
 
 # 26082024python_ = 'batch started' #Invalid
-_allowed = 'valid' #valid but it is private
-__allowed = "valid" #strongly private
-print(_allowed)
-print(__allowed)
+# _allowed = 'valid' #valid but it is private
+# __allowed = "valid" #strongly private
+# print(_allowed)
+# print(__allowed)
 
-amount = 35000 #valid
+# amount = 35000 #valid
 # amou$nt = 1000000 #Invalid
 
-name = 'SAI KUMAR PERAMSETTY' #valid
-NAME = 'sai kumar peramsetty' #valid
-print(name)
-print(NAME)
+# name = 'SAI KUMAR PERAMSETTY' #valid
+# NAME = 'sai kumar peramsetty' #valid
+# print(name)
+# print(NAME)
 
 # name$ = 'SAI KUMAR' #Invalid
-N2AM8E = 'sai kumar p' #valid
+# N2AM8E = 'sai kumar p' #valid
 # print(name$)
-print(N2AM8E)
+# print(N2AM8E)
 
 # Reserved Words:
 # --> It has a special meaning and purpose.
 # --> We cannot use them as identifiers.
 
-import keyword
-print(keyword.kwlist)
+'''import keyword
+print(keyword.kwlist)'''
+
+# Comments in Python:
+# Single Line Comment: #
+# Multiline Comments: ''' ''' or """ """
+
+# Indentation:
+#  It means define the structure and flow of code
+# Indentation for if statement:
+# if 28 > 29:
+# print('29 is biger than 28')
+
+# def myIndentation():
+#     if 29 < 28:
+#         print('29 is biger than 28')
+# myIndentation()
+
+# Variables and Constants:
+# Variable in python is a container that can hold values of any valid python datatype.
+# variableName = value 
+myVar = 'varible'
+myVar = 'sai'
+myVarName = 'sai'
+print(myVar)
+print(myVar)
+print(myVarName)
+
+# Variable Naming Rules:
+# camelCase = variables, functions
+# PascalCase = classes
+# snake_case = my_module.py
+
+# Constants: WE USED IN CONFIGARATION SETTINGS
+# if we want to mention anything as constant means
+# Ex:
+MAX_WEIGHT = 60
+MIN_WEIGHT = 45
