f4c37ae802367e8daf3ccf1342ae4dab0697e6bf
saikumarperamsetty <saikumar.peramsetty77@gmail.com>
2024-09-02 16:18:42 +0530

    update

--- Python/basics/operators/operators.py
+++ Python/basics/operators/operators.py
@@ -68,3 +68,84 @@ print(not " ") #true
 age = 14
 vote = age >= 18
 print(not vote)
+
+# 6. Bitwise Operators:
+# & = if both digits are 1 then the result is 1 else 0
+# | = if atleast one of the digits is 1 then the result is else 0
+# ^ = both digits must be different then the result is 1 else 0
+# ~ = complementary
+# << = bitwise left shift
+# >> = bitwise right shift
+
+# bitwise & operator:
+# Ex:
+print(4&5) #4
+# 00000100
+# 00000101
+# res = 00000100 ==> 4
+
+# bitwise & operator:
+# Ex:
+print(4&5) #4
+# 00000100
+# 00000101
+# res = 00000100 ==> 4
+
+print(5&8) #0
+# 00000101
+# 00001000
+# res = 00000000 ==> 0
+
+# bitwise | operator:
+print(5|8) #13
+# 00000101
+# 00001000
+# res = 00001101 ==> 13 if we convert into binary-> 0B1101 ==> 13
+
+# bitwise ^ operator:
+print(3^5) #6
+# 00000011
+# 00000101
+# res = 00000110 ==> 6 if we convert into binary-> 0B0110 ==> 6
+
+# bitwise ~ operator:
+print(~5) #-6
+# 00000101 => (binary representation)
+# 11111010 => flipping
+# 00000101+1(add one number) = -0110 ==> -6
+
+# << = bitwise left shift Operator:
+print(10<<2)
+# 00001010 => 10
+# 00101000 => after applying left shift (remove left side 00 and add right side 00)
+print(0b00101000)
+
+# >> = bitwise right shift Operator:
+print(10>>2)
+# 00001010 => 10
+# 00000010 => after applying right shift (remove right side values of two digits and add left side values of two 00s)
+print(0b00000010)
+
+# 5. Ternary Operators: it is modern way of using if else
+# syntax: value_if_true if condition else value_if_false
+x = 28
+y = 29
+result = x if x < y else y
+print(result) #28
+
+a = 'apple'
+b = 'banana'
+res = a if len(a) < len(b) else b #apple
+res = a if len(a) > len(b) else b #banana
+print(res)
+
+# How to check Number is Even or Odd number?
+num = 28
+resul = 'even' if num%2 == 0 else 'odd'
+print(resul)
+
+# How to check Number is divisible by 2 or not?
+num = 28
+resul = 'divisable' if num%2 == 0 else 'not divisable'
+print(resul)
+
