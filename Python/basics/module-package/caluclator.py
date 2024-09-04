f79cb129a3bc826c1077aa5eece06d51ddb0ff30
saikumarperamsetty <saikumar.peramsetty77@gmail.com>
2024-09-04 21:12:48 +0530

    caluclator with module completed

---
+++ Python/basics/module-package/caluclator.py
@@ -0,0 +1,10 @@
+z = 28
+
+def add(a,b):
+    print('Sum = ',a+b)
+def sub(a,b):
+    print('subtract = ',a-b)
+def mul(a,b):
+    print('mul = ',a*b)
+def div(a,b):
+    print('div = ',a/b)
---
+++ Python/basics/module-package/main.py
@@ -0,0 +1,16 @@
+# import caluclator as calc
+from caluclator import *
+
+print(z)
+
+#with alias name
+# print(calc.add(20,8))
+# print(calc.sub(20,8))
+# print(calc.mul(20,8))
+# print(calc.div(20,8))
+
+#Without alias name
+print(add(20,8))
+print(sub(20,8))
+print(mul(20,8))
+print(div(20,8))
