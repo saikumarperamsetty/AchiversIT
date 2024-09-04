d0eb234b95ed3d9d6c8b89a888d8e686cc9538ae
saikumarperamsetty <saikumar.peramsetty77@gmail.com>
2024-09-04 11:44:13 +0530

    module-package started

---
+++ Python/basics/module-package/modulepackage.py
@@ -0,0 +1,10 @@
+# Module-Package in Python:
+# module - is a file 
+# python module - is a python file 
+
+x = 28
+
+def sum(a,b):
+    print('Sum = ',a+b)
+def subtract(a,b):
+    print('subtract = ',a-b)
---
+++ Python/basics/module-package/xyz.py
@@ -0,0 +1,5 @@
+import modulepackage
+
+print(modulepackage.x)
+modulepackage.sum(20,8)
+modulepackage.subtract(20,9)
