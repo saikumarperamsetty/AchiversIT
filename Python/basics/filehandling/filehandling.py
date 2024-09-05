b2937bdfe80c159a49eeca79f798461951d97a57
saikumarperamsetty <saikumar.peramsetty77@gmail.com>
2024-09-05 23:09:17 +0530

    update

--- Python/basics/filehandling/filehandling.py
+++ Python/basics/filehandling/filehandling.py
@@ -1,5 +1,15 @@
-f = open('sample.txt','r')
-f.write('Learning C is also Easy\n')
-read = f.read()
-print(read)
-f.close()
+# f = open('sample.txt','r')
+# f.write('Learning C is also Easy\n')
+
+# # to read the file Data
+# read = f.read()
+# print(read)
+
+# f.close()
+
+f1 = open('mydetails.txt','r')
+f1.write('Hi, I am Sai Kumar\n')
+f1.writelines('I want to become Python Developer\n')
+readData = f1.read()
+print(readData)
+f1.close()
