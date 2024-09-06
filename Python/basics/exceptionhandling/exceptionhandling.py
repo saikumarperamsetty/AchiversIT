435727ba60aa8013f23221dbb56231b7c71cf815
saikumarperamsetty <saikumar.peramsetty77@gmail.com>
2024-09-06 21:53:49 +0530

    exception handling

---
+++ Python/basics/exceptionhandling/exceptionhandling.py
@@ -0,0 +1,14 @@
+# Exception = An event that inturrept the flow of program
+# types = (ZeroDivisionError, TypeError, ValueError)
+
+# Ex:1
+# ZeroDivisionError:
+1 / 0       #ZeroDivisionError
+
+# Ex:2
+# TypeError
+1 + "python programming"    #TypeError
+
+# Ex:3
+# ValueError
+int("python exception handling")    #ValueError
