df0095737e030bf4bbde9998a5f43e4d74b775a8
saikumarperamsetty <saikumar.peramsetty77@gmail.com>
2024-09-06 22:21:56 +0530

    update

--- Python/basics/exceptionhandling/exceptionhandling.py
+++ Python/basics/exceptionhandling/exceptionhandling.py
@@ -3,23 +3,36 @@
 
 # Ex:1
 # ZeroDivisionError:
-1 / 0       #ZeroDivisionError
+# 1 / 0       #ZeroDivisionError
 
 # Ex:2
 # TypeError
-1 + "python programming"    #TypeError
+# 1 + "python programming"    #TypeError
 
 # Ex:3
 # ValueError
-int("python exception handling")    #ValueError
+# int("python exception handling")    #ValueError
 
 # Ex:4 What is try, except, finally in python?
-try:
-    number = int(input("Enter a Number:"))
-    print(1 / number)
-except ZeroDivisionError:
-    print("You cant't devide by Zero!")
-except TypeError:
-    print("Please Enter numbers only")
-finally:
-    print("This will do the Cleanup Activities..!")
+# try:
+#     number = int(input("Enter a Number:"))
+#     print(1 / number)
+# except ZeroDivisionError:
+#     print("You cant't devide by Zero!")
+# except TypeError:
+#     print("Please Enter numbers only")
+# finally:
+#     print("This will do the Cleanup Activities..!")
+
+#Ex: 5
+def devide(x,y):
+    try:
+        result = x / y
+    except ZeroDivisionError:
+        print("You can't devide by ZERO")
+    else:
+        print("result = ", result)
+    finally:
+        print("finally Block is Excuted at the end!")
+devide(28,7)
+devide(28,2)
