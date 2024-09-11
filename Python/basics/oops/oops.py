0443fa062e20493209a1f1c0cc6f434668ed75e4
saikumarperamsetty <saikumar.peramsetty77@gmail.com>
2024-09-11 11:17:12 +0530

    update

--- Python/basics/oops/oops.py
+++ Python/basics/oops/oops.py
@@ -1,7 +1,9 @@
 #Ex:1
 class Student:
 # for creating the Student Object
+#Ex:2
     def __init__(self,name,age,subject,marks,city):
+        #Ex:1
         # self.name = 'sai'
         # self.age = 28
         # self.subject = 'python'
@@ -23,10 +25,12 @@ class Student:
         print('Hi My city is ', self.city)
 
 # we are creating instance off Student
+#Ex:1
 # accecing the values directly with the method
 # student1 = Student()
 # student1.details()
 
+#Ex:2
 # accecing the values directly with in the init method
 stu1 = Student('Alice',24,'java',59,'USA')
 print(stu1.name)
@@ -43,3 +47,16 @@ print('\n')
 stu3 = Student('SANIO ANDERSEN',19,'Python',45,'UK')
 print(stu3.name)
 stu3.details()
+
+
+# Constructor Function( init is Mandatory)
+# Instance Function( init is not Mandatory otherthan any name you can take)
+# Ex:3
+print('\n')
+class className():
+    def __init__(self):
+        print('Constructor Function Excuted.')
+    def instance(self):
+        print('Instance Function Excuted..')
+obj = className()
+obj.instance()
