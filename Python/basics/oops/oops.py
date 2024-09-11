8a528e7648d8c9aa5161e79cbb591ea616b6270f
saikumarperamsetty <saikumar.peramsetty77@gmail.com>
2024-09-11 10:47:28 +0530

    update

--- Python/basics/oops/oops.py
+++ Python/basics/oops/oops.py
@@ -1,12 +1,18 @@
 #Ex:1
 class Student:
 # for creating the Student Object
-    def __init__(self):
-        self.name = 'sai'
-        self.age = 28
-        self.subject = 'python'
-        self.marks = 76
-        self.city = 'Bangalore'
+    def __init__(self,name,age,subject,marks,city):
+        # self.name = 'sai'
+        # self.age = 28
+        # self.subject = 'python'
+        # self.marks = 76
+        # self.city = 'Bangalore'
+
+        self.name = name
+        self.age = age
+        self.subject = subject
+        self.marks = marks
+        self.city = city
 
 # for accepting the Student Object values
     def details(self):
@@ -18,5 +24,22 @@ class Student:
 
 # we are creating instance off Student
 # accecing the values directly with the method
-student1 = Student()
-student1.details()
+# student1 = Student()
+# student1.details()
+
+# accecing the values directly with in the init method
+stu1 = Student('Alice',24,'java',59,'USA')
+print(stu1.name)
+stu1.details()
+print('\n')
+
+# accecing the values directly with in the init method
+stu2 = Student('Bob',21,'dotnet',59,'FRANCE')
+print(stu2.name)
+stu2.details()
+print('\n')
+
+# accecing the values directly with in the init method
+stu3 = Student('SANIO ANDERSEN',19,'Python',45,'UK')
+print(stu3.name)
+stu3.details()
