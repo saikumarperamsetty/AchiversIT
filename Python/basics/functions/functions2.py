cf7c2af286aa2437d17d3742df3055190b20fe7e
saikumarperamsetty <saikumar.peramsetty77@gmail.com>
2024-09-12 23:04:33 +0530

    update

--- Python/basics/functions/functions2.py
+++ Python/basics/functions/functions2.py
@@ -53,13 +53,23 @@
 # definition: declaring a function without a name are called Anonymous function or lambda function 
 
 # for Ex: Normal Function 
-def nf(h):
-    return h*h
+# def nf(h):
+#     return h*h
 
 # for Anonymous Function or lambda function
-lambda f: f*f
+# lambda f: f*f
 
-# Ex: by using lambda Expression find Square of a Number?
-square = lambda sq: sq*sq
-print('The Square of 28 is ',square(28))
-print('The Square of 82 is ',square(82))
+# Ex:1 by using lambda Expression find Square of a Number?
+# square = lambda sq: sq*sq
+# print('The Square of 28 is ',square(28))
+# print('The Square of 82 is ',square(82))
+
+# Ex:2 by using lambda Expression find sum of a two Numbers?
+sumOfTwoNums = lambda m,n : m+n
+print('Addition of 20 and 8 is ',sumOfTwoNums(20,8))
+print('addition of 4 and 13 is ',sumOfTwoNums(4,13))
+
+# Ex:3 by using lambda Expression find biggest of a two Numbers?
+biggestOfTwoNums = lambda p,q : p if p>q else q
+print('between 20 and 8, the bigger Num is ',biggestOfTwoNums(20,8))
+print('between 28 and 82, the bigger Num is ',biggestOfTwoNums(28,82))
