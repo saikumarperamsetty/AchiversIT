0bc4d6b5e41c636e564aceab2e4404875f4b3202
saikumarperamsetty <saikumar.peramsetty77@gmail.com>
2024-09-12 19:19:25 +0530

    update

--- Python/basics/functions/function1.py
+++ Python/basics/functions/function1.py
@@ -1,4 +1,22 @@
+# Syntax of Function
+# def functionName():
+#     -----
+
 # Ex:1
-def greet():
-    print('Hii Good Evening to All..! I am From greet Function')
-greet()
+def greet(name):
+    print('Hii',name,'Good Evening to All..! I am From greet Function')
+greet('sai')
+
+#Ex:2
+def square(x):
+    print('\nSquare of ',x ,'is =', x * x)
+square(7)
+
+#Ex:3
+print('\n')
+def add(i,j):
+    print('addition = ',i+j)
+def mul(i,j):
+    print('multiplication = ',i*j)
+add(20,8)
+mul(28,33)
