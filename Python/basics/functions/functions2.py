13b02adbdf9309baa1453752f113ef9e23f35eff
saikumarperamsetty <saikumar.peramsetty77@gmail.com>
2024-09-12 21:56:39 +0530

    functions2 started

---
+++ Python/basics/functions/functions2.py
@@ -0,0 +1,9 @@
+# Global Variable:
+# Ex:
+p=28    #global variable
+def m1():
+    print(p)    #accessing global variable in m1
+def m2():
+    print(p)    #accessing global variable in m2
+m1()
+m2()
