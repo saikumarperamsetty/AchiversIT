efec304414bc656e1d45a53a6c13b1414338b88c
saikumarperamsetty <saikumar.peramsetty77@gmail.com>
2024-09-12 22:01:12 +0530

    update

--- Python/basics/functions/functions2.py
+++ Python/basics/functions/functions2.py
@@ -1,9 +1,21 @@
 # Global Variable:
 # Ex:
-p=28    #global variable
-def m1():
-    print(p)    #accessing global variable in m1
-def m2():
-    print(p)    #accessing global variable in m2
-m1()
-m2()
+# p=28    #global variable
+# def m1():
+#     print(p)    #accessing global variable in m1
+# def m2():
+#     print(p)    #accessing global variable in m2
+# m1()
+# m2()
+
+# Local Variable:
+# Ex:
+def m3():
+    z=12    #local variable
+    print(z)
+def m4():
+    y=99
+    print(y)    #local variable
+    # print(z)    #Other function variable not able to access in m4
+m3()
+m4()
