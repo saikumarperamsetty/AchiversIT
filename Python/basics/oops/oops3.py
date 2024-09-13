9859034aafc0a8b9f17b65f7de96c7ce272f30ac
saikumarperamsetty <saikumar.peramsetty77@gmail.com>
2024-09-13 15:47:46 +0530

    update

--- Python/basics/oops/oops3.py
+++ Python/basics/oops/oops3.py
@@ -16,37 +16,55 @@
 # car1.display()
 
 # Ex: 2
-class Student:
-    def __init__(self,name,age,marks):
-        self.name = name
-        self.age = age
-        self.marks = marks
-    def display(self):
-        print(f"Student Name:{self.name}")
-        print(f'Student Age:{self.age}')
-        print(f"Student Marks:{self.marks}")
-    def grade(self):
-        if self.marks>90:
-            return 'A'
-        elif 80 <= self.marks <90:
-            return 'B'
-        elif 70 <= self.marks <80:
-            return 'C'
-        elif 60 <= self.marks <70:
-            return 'D'
-        else:
-            return 'Fail'
+# class Student:
+#     def __init__(self,name,age,marks):
+#         self.name = name
+#         self.age = age
+#         self.marks = marks
+#     def display(self):
+#         print(f"Student Name:{self.name}")
+#         print(f'Student Age:{self.age}')
+#         print(f"Student Marks:{self.marks}")
+#     def grade(self):
+#         if self.marks>90:
+#             return 'A'
+#         elif 80 <= self.marks <90:
+#             return 'B'
+#         elif 70 <= self.marks <80:
+#             return 'C'
+#         elif 60 <= self.marks <70:
+#             return 'D'
+#         else:
+#             return 'Fail'
         
-student1 = Student('sai',28,83)
-student2 = Student('suraj',33,93)
-student3 = Student('madhavi',25,65)
-student4 = Student('Alice',25,28)
+# student1 = Student('sai',28,83)
+# student2 = Student('suraj',33,93)
+# student3 = Student('madhavi',25,65)
+# student4 = Student('Alice',25,28)
 
-student1.display()
-print(f'Grade: {student1.grade()}')
-student2.display()
-print(f'Grade: {student2.grade()}')
-student3.display()
-print(f'Grade: {student3.grade()}')
-student4.display()
-print(f'Grade: {student4.grade()}')
+# student1.display()
+# print(f'Grade: {student1.grade()}')
+# student2.display()
+# print(f'Grade: {student2.grade()}')
+# student3.display()
+# print(f'Grade: {student3.grade()}')
+# student4.display()
+# print(f'Grade: {student4.grade()}')
+
+
+# 2. class methods
+# EX:3
+class Car:
+    total_cars = 0
+    def __init__(self,brand,model):
+        self.brand = brand
+        self.model = model
+        Car.total_cars +=1
+    @classmethod
+    def total(cls):
+        print(f'Total Cars: {cls.total_cars}')
+car1 = Car('Maruti','jreky')
+car2 = Car('Toyota','krim')
+car3 = Car('Skoda','prino')
+car4 = Car('Tata','qeril')
+Car.total()
