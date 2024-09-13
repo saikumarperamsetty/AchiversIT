963cef8ce364d5411d4a5605daa4b8c478b1c943
saikumarperamsetty <saikumar.peramsetty77@gmail.com>
2024-09-13 23:11:39 +0530

    multiple inheritance completed

--- Python/basics/oops/oops4.py
+++ Python/basics/oops/oops4.py
@@ -1,11 +1,27 @@
 # Task 1: Single Inheritance
-class Animal:
-    def sound(self):
-        print('its sounding')
-class Dog(Animal):
-    def sound(self):
-        print('dog barks..')
-animal = Animal()
-animal.sound()
-dog = Dog()
-dog.sound()
+# class Animal:
+#     def sound(self):
+#         print('its sounding')
+# class Dog(Animal):
+#     def sound(self):
+#         print('dog barks..')
+# animal = Animal()
+# animal.sound()
+# dog = Dog()
+# dog.sound()
+
+# Task 2: Multiple Inheritance
+class Mother:
+    def motherProfession(self):
+        print('Mother is like God..')
+class Father:
+    def fatherProfession(self):
+        print('Father is Like Instructor..')
+class Child(Mother,Father):
+    def childProfession(self):
+        print('Child is Always not Listening Parents Words')
+
+child = Child() 
+child.motherProfession()
+child.fatherProfession()
+child.childProfession()
