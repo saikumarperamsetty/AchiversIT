1e7dd05189bd8f5492e956c960556d4c087cbac2
saikumarperamsetty <saikumar.peramsetty77@gmail.com>
2024-09-14 08:54:00 +0530

    task3 completed

--- Python/basics/oops/oops4.py
+++ Python/basics/oops/oops4.py
@@ -11,17 +11,33 @@
 # dog.sound()
 
 # Task 2: Multiple Inheritance
-class Mother:
-    def motherProfession(self):
-        print('Mother is like God..')
-class Father:
-    def fatherProfession(self):
-        print('Father is Like Instructor..')
-class Child(Mother,Father):
-    def childProfession(self):
-        print('Child is Always not Listening Parents Words')
+# class Mother:
+#     def motherProfession(self):
+#         print('Mother is like God..')
+# class Father:
+#     def fatherProfession(self):
+#         print('Father is Like Instructor..')
+# class Child(Mother,Father):
+#     def childProfession(self):
+#         print('Child is Always not Listening Parents Words')
 
-child = Child() 
-child.motherProfession()
-child.fatherProfession()
-child.childProfession()
+# child = Child() 
+# child.motherProfession()
+# child.fatherProfession()
+# child.childProfession()
+
+# Task 3: Multilevel Inheritance 
+class Vehicle:
+    def general_usage(self):
+        print('Vehicle is for general usage.')
+class Car(Vehicle):
+    def special_usage(self):
+        print('Car is for personal usage..')
+class ElectricCar(Car):
+    def eco_usage(self):
+        print('ElectricCar is for environment friendly...')
+
+e_car = ElectricCar()
+e_car.eco_usage()
+e_car.special_usage()
+e_car.general_usage()
