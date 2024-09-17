fab9f52f21c667a80b735383039d49ee18ab1069
saikumarperamsetty <saikumar.peramsetty77@gmail.com>
2024-09-17 22:52:13 +0530

    update

--- Python/basics/functions/fun4assignment.py
+++ Python/basics/functions/fun4assignment.py
@@ -2,28 +2,28 @@
 # given number n. Test the generator by printing the first 10 numbers in 
 # the sequence.
 # FIBONACCI SERIES UPTO GIVEN n?
-# def fibonacci_series():
-#     a,b = 0,1
-#     while True:
-#         yield a
-#         a,b = b, a+b
-# for i in fibonacci_series():
-#     if i > 50:
-#         break
-#     print(i)
+def fibonacci_series():
+    a,b = 0,1
+    while True:
+        yield a
+        a,b = b, a+b
+for i in fibonacci_series():
+    if i > 50:
+        break
+    print(i)
 
 
 # 2. Create a generator that yields an infinite sequence of natural numbers 
 # starting from 1. Use next() to print the first 20 numbers.
-# def infinite_sequence():
-#     a = 0
-#     while True:
-#         a +=1
-#         yield a
-# for i in infinite_sequence():
-#     if i >= 21:
-#         break
-#     print(i)
+def infinite_sequence():
+    a = 0
+    while True:
+        a +=1
+        yield a
+for i in infinite_sequence():
+    if i >= 21:
+        break
+    print(i)
 
 
 # 3. Write a generator that yields prime numbers up to a given limit. 
