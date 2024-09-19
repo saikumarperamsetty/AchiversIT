b6a59ddd9b186c7d052ab8cb840791dbb2544c4b
saikumarperamsetty <saikumar.peramsetty77@gmail.com>
2024-09-19 22:36:03 +0530

    oops6 abstraction and Payment System for E-commerse Website COMPLETED

--- Python/basics/oops/oops6.py
+++ Python/basics/oops/oops6.py
@@ -1,26 +1,26 @@
 # Abstraction in Python:
 # Ex:1 Basic Example for Abstraction in Python?
-# from abc import ABC, abstractmethod
-# class Shape(ABC):
-#     @abstractmethod
-#     def area(self):
-#         pass
-#     @abstractmethod
-#     def perimeter(self):
-#         pass
+from abc import ABC, abstractmethod
+class Shape(ABC):
+    @abstractmethod
+    def area(self):
+        pass
+    @abstractmethod
+    def perimeter(self):
+        pass
 
-# class Rectangle(Shape):
-#     def __init__(self,width,height):
-#         self.width = width
-#         self.height = height
-#     def area(self):
-#         return self.width * self.height
-#     def perimeter(self):
-#         return 2 *(self.width + self.height)
+class Rectangle(Shape):
+    def __init__(self,width,height):
+        self.width = width
+        self.height = height
+    def area(self):
+        return self.width * self.height
+    def perimeter(self):
+        return 2 *(self.width + self.height)
     
-# rectangle = Rectangle(28,29)
-# print(rectangle.area())
-# print(rectangle.perimeter())
+rectangle = Rectangle(28,29)
+print(rectangle.area())
+print(rectangle.perimeter())
 
 
 # Ex:2 Payment System for E-commerse Website?
