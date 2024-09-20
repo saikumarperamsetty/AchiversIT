4d6eb44aab05b4eaa104f8ecf6491cf4ebf7646b
saikumarperamsetty <saikumar.peramsetty77@gmail.com>
2024-09-20 22:15:56 +0530

    functions5 Basic Example of Iterative Approach in Python? completed

--- Python/basics/functions/functions5.py
+++ Python/basics/functions/functions5.py
@@ -21,7 +21,8 @@
 # 2. Indirect recursion
 # 3. Tail recursion
 
-# 2. Basic Example of Indirect recursion in Python?
+
+# Ex:2 Basic Example of Indirect recursion in Python?
 # def func1(n):
 #     if n > 0:
 #         print(n)
@@ -32,11 +33,22 @@
 #         func1(n // 2)
 # func1(10) #output = 10 9 4 3 1
 
-# 2. Basic Example of tail recursion in Python?
-def tail_recursion_factorial(n,accumalator=1):
-    if n == 0:
-        return accumalator
-    else:
-      return  tail_recursion_factorial(n-1,n*accumalator)
-result = tail_recursion_factorial(5)
-print(result)   #output = 120
+
+# Ex:3 Basic Example of tail recursion in Python?
+# def tail_recursion_factorial(n,accumalator=1):
+#     if n == 0:
+#         return accumalator
+#     else:
+#       return  tail_recursion_factorial(n-1,n*accumalator)
+# result = tail_recursion_factorial(5)
+# print(result)   #output = 120
+
+
+# Ex:4 Basic Example of Iterative Approach in Python?
+def iterative_factorial(n):
+    result = 1
+    for i in range(1, n+1):
+        result *= i
+    return result
+res = iterative_factorial(5)
+print(res)  #output = 120
