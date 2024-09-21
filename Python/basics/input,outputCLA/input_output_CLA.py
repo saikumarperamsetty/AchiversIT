70700a1e2b75a8eeef2247f79f9e730127b09c21
saikumarperamsetty <saikumar.peramsetty77@gmail.com>
2024-09-21 23:11:22 +0530

    string example in input,output and CLA completed

--- Python/basics/input,outputCLA/input_output_CLA.py
+++ Python/basics/input,outputCLA/input_output_CLA.py
@@ -1,18 +1,26 @@
 # Input, Output and CLA
-Input = raw_input('Enter Some Value')   #Python 2 series
-print(Input)
+# Input = raw_input('Enter Some Value')   #Python 2 series
+# print(Input)
 
-Input = input('Enter Some value:')   #Python 3 series
-print(type(Input))  #by Default type is str
+# Input = input('Enter Some value:')   #Python 3 series
+# print(type(Input))  #by Default type is str
 
-x = input('Enter First Number:')    #10
-y = input('Enter Second Number:')   #20
-# # print(x+y)      # output = 1020 --> it treated like Concatination
-p = int(x)
-q = int(y)
+# Ex:1
+# x = input('Enter First Number:')    #10
+# y = input('Enter Second Number:')   #20
+# # # print(x+y)      # output = 1020 --> it treated like Concatination
+# p = int(x)
+# q = int(y)
 
 # #Approach-1 to print input
-print('The Sum is: ',p+q)      # output = 30
+# print('The Sum is: ',p+q)      # output = 30
 
+# Ex:2
 #Approach-2 to print input
-print('The Sum is: ',int(input('Enter First Number:'))+int(input('Enter Second Number:')))  # 28 29 = 57
+# print('The Sum is: ',int(input('Enter First Number:'))+int(input('Enter Second Number:')))  # 28 29 = 57
+
+
+# Ex:3
+str1 = input('Enter First String:')    #SAI
+str2 = input('Enter Second String:')   #KUMAR
+print('Result :', str1+str2)    #SAIKUMAR
