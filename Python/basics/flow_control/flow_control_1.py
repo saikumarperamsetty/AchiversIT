3878d14d203127b738f85ccd1d3b17c2cea18bcd
saikumarperamsetty <saikumar.peramsetty77@gmail.com>
2024-09-22 18:13:55 +0530

    if else Example in Flow Control-1? completed

---
+++ Python/basics/flow_control/flow_control_1.py
@@ -0,0 +1,14 @@
+# Flow Control-1:
+# Flow Control: flow is the order in which the statements are executed.
+# Conditions: if elif else 
+# Loops : for, while
+# Transfer Statements or Loop Controls: break, continue and pass
+
+
+# Conditions: if elif else 
+# Ex:1 if else
+marks = 83
+if marks >= 35:
+    print('You Passed the Exam.!!')
+else:
+    print('Sorry, You Failed the Exam...!')
