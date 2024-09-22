828026f36aac8d344a6fd549249089e5a4dfba17
saikumarperamsetty <saikumar.peramsetty77@gmail.com>
2024-09-22 18:27:20 +0530

    if else elif Example Completed in FlowControl1?

--- Python/basics/flow_control/flow_control_1.py
+++ Python/basics/flow_control/flow_control_1.py
@@ -7,8 +7,20 @@
 
 # Conditions: if elif else 
 # Ex:1 if else
-marks = 83
-if marks >= 35:
-    print('You Passed the Exam.!!')
+# marks = 83
+# if marks >= 35:
+#     print('You Passed the Exam.!!')
+# else:
+#     print('Sorry, You Failed the Exam...!')
+
+
+# Ex:2 if else elif
+temp = 4
+if temp >= 28:
+    print("It's, Hot Outside")
+elif temp >= 12:
+    print("It's, Cool Outside")
+elif temp >= 5:
+    print("It's, Freezing Cold Outside")
 else:
-    print('Sorry, You Failed the Exam...!')
+    print("Don't go Outside..?")
