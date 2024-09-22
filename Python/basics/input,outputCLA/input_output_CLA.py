ee4fb8a886bd154b8ac0e4935c2d3b9a61237eb6
saikumarperamsetty <saikumar.peramsetty77@gmail.com>
2024-09-22 13:06:12 +0530

    Destructuring And end= operator in Python? Completed

--- Python/basics/input,outputCLA/input_output_CLA.py
+++ Python/basics/input,outputCLA/input_output_CLA.py
@@ -65,14 +65,42 @@
 
 
 # Ex:7  How to use print() method in different ways in PYTHON?
-print('SaiKumar')   # output = SaiKumar
-print('Sai\tKumar') # output = Sai  Kumar (\t = its giving tab space=)
-print('Sai\nKumar') # output = Sai  \nKumar (\n = its coming in new line)
+# print('SaiKumar')   # output = SaiKumar
+# print('Sai\tKumar') # output = Sai  Kumar (\t = its giving tab space=)
+# print('Sai\nKumar') # output = Sai  \nKumar (\n = its coming in new line)
 
-# repetion_operator: (*) means one value should be String and value Should be Int 
-print('SAI KUMAR'*3)    # output = SAI KUMARSAI KUMARSAI KUMAR (it will print 3times)
+# # repetion_operator: (*) means one value should be String and value Should be Int 
+# print('SAI KUMAR'*3)    # output = SAI KUMARSAI KUMARSAI KUMAR (it will print 3times)
 
-# concatination_operator: (+) it will add Two Strings 
-print('SAI'+'KUMAR')    # output = SAIKUMAR
+# # concatination_operator: (+) it will add Two Strings 
+# print('SAI'+'KUMAR')    # output = SAIKUMAR
 
-print('SAI KUMAR'+10)    # output = it will Give TYPE ERROR --> TypeError: can only concatenate str (not "int") to str
+# print('SAI KUMAR'+10)    # output = it will Give TYPE ERROR --> TypeError: can only concatenate str (not "int") to str
+
+# comma operator(,) ==>here comma(,) seperates the space( ) --> This Comma(,) not there in JavaScript, But we can we can use both comma(,) and plus(+) in Python.
+print('PEARMSETTY','SAI KUMAR')     # output = PEARMSETTY SAI KUMAR (here comma(,) seperates the space( ) in the output)
+
+# Ex:8  How to use Destructuring in Python?
+p,q,r = 28,29,30
+print('The Destructered Values are: ', p,q,r)   # output = The Destructered Values are:  28 29 30  # here comma(,) giving the space in the output
+
+# Ex:9  How to use end= operator in Python?
+print('PERAMSETTY',end=' ')     #   here end= operator is it will all print results in Same line
+print('SAI',end=' ')            # here end= operator is it will all print results in Same line
+print('KUMAR')              # output = PERAMSETTY SAI KUMAR
+
+# Ex:10     print() for list,tuple and int?
+# list
+list2 = (10,20,30,40,50)
+print(list2)
+print(type(list2))
+
+# tuple
+tuple1 = 10,20,30,40,50
+print(tuple1)
+print(type(tuple1))
+
+# int
+int1 =28
+print(int1)
+print(type(int1))
