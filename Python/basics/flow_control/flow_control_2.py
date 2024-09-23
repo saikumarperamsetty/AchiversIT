5183696021394bd5298c55ab5cc9b67bc9cff2a8
saikumarperamsetty <saikumar.peramsetty77@gmail.com>
2024-09-23 14:28:24 +0530

    flow_control_2  How to use Nested Loops?

--- Python/basics/flow_control/flow_control_2.py
+++ Python/basics/flow_control/flow_control_2.py
@@ -2,12 +2,18 @@
 # definition = writing one condition inside another condition are called Nested Condition
 
 # Ex:1 How to check a person is eligible for marriage and Adult by using Nested if else Condition?
-age = 28
-if age >= 18:
-    print('You are Adult')
-    if age >= 21:
-        print('You are eligible for Marriage')
-    else:
-        print('You are Not eligible for Marriage')
-else:
-    print('You are minor')
+# age = 28
+# if age >= 18:
+#     print('You are Adult')
+#     if age >= 21:
+#         print('You are eligible for Marriage')
+#     else:
+#         print('You are Not eligible for Marriage')
+# else:
+#     print('You are minor')
+
+
+# Ex:2 How to use Nested Loops?
+for i in range(4):
+    for j in range(4):
+        print('i',i,'j',j)
