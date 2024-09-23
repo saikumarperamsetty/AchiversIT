bfdfc73eeb7f0f24e22ee34f3a7346ff46874c8e
saikumarperamsetty <saikumar.peramsetty77@gmail.com>
2024-09-23 19:59:04 +0530

    list_part1  How apply for,while loops in List in Python?

--- Python/basics/list/list_part1.py
+++ Python/basics/list/list_part1.py
@@ -11,38 +11,76 @@
 # 5 ways to create a List in Python?
 # Ex:1 By Directly using the empty Square Brackets and append values to it?
 # Type-1:
-l = []
-l.append('PERAMSETTY')
-l.append('SAI')
-l.append('KUMAR')
-l.append(28)
-print(l)
-print(type(l))
+# l = []
+# l.append('PERAMSETTY')
+# l.append('SAI')
+# l.append('KUMAR')
+# l.append(28)
+# print(l)
+# print(type(l))
 
-# Ex:2 If we Know list values in Advance?
-# Type-2:
-l = [10,20,30,40,50]
-print(l)
-print(type(l))
-
-
-# Ex:3 by splitting the String values?
-# Type-3:
-str = 'Learning Python is Very Easy'
-s = str.split()
-print(s)
-print(type(s))
+# # Ex:2 If we Know list values in Advance?
+# # Type-2:
+# l = [10,20,30,40,50]
+# print(l)
+# print(type(l))
 
 
-# Ex:4 by using list function?
-# Type-4:
-l = list([28,30,29])
-print(l)
-print(type(l))
+# # Ex:3 by splitting the String values?
+# # Type-3:
+# str = 'Learning Python is Very Easy'
+# s = str.split()
+# print(s)
+# print(type(s))
 
 
-# Ex:5 by using eval function?
-# Type-5:
-l = eval(input('Enter list values(by using eval):'))
-print(l)
-print(type(l))
+# # Ex:4 by using list function?
+# # Type-4:
+# l = list([28,30,29])
+# print(l)
+# print(type(l))
+
+
+# # Ex:5 by using eval function?
+# # Type-5:
+# l = eval(input('Enter list values(by using eval):'))
+# print(l)
+# print(type(l))
+
+
+# Ex:6 How to use indexing in list?
+# l = [10,20,30,40,50]
+# print(l[3])
+# print(l[1])
+# print(l[-1])
+
+
+# Ex:7 How to use slicing in list?
+# slice = [start:end:step]
+# l = [10,20,30,40,50,28,21,14,92,24,67]
+# sliced = l[4:-3]
+# print(sliced)
+
+
+# Ex:8 How to use Traversing in list?
+l = [10,22,35,48,55,28,21,11,92,25,67]
+
+# printing values by using for loop
+for i in l:
+    print(i)
+
+# printing values by using while loop
+i = 0
+while i < len(l):
+    print(l[i])
+    i += 1
+
+# # for Even numbers
+for i in l:
+    if i % 2 == 0:
+        print(i)
+
+# # for odd numbers
+for i in l:
+    if i % 2 != 0:
+        print(i)
