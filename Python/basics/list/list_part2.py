f243cd318ecb715c70af06ed8d84e36f9a9cecb6
saikumarperamsetty <saikumar.peramsetty77@gmail.com>
2024-09-24 16:01:18 +0530

    list_part2  max(), min() and sum() methods on list in Python?

--- Python/basics/list/list_part2.py
+++ Python/basics/list/list_part2.py
@@ -1,7 +1,21 @@
 # Ex:27 Membership operators on list in Python?
 #  Membership operators = in, not in
-l = [10,30,20,90,40,50]
-print(90 in l)          # output = True
-print(180 in l)         # output = False
-print(60 not in l)      # output = True
-print(30 not in l)      # output = False
+# l = [10,30,20,90,40,50]
+# print(90 in l)          # output = True
+# print(180 in l)         # output = False
+# print(60 not in l)      # output = True
+# print(30 not in l)      # output = False
+
+
+# Ex:28 max(), min() and sum() methods on list in Python?
+# max()
+l = [10,90,40,50,30]
+print(max(l))           # output = 90
+
+# min()
+l = [10,90,40,50,30]
+print(min(l))           # output = 10
+
+# sum()
+l = [10,20,60,50,40]
+print(sum(l))           # output = 180
