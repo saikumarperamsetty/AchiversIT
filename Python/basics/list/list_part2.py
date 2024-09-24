5ec7c7ee820c60fcd7b052af4cb2b57d106c2595
saikumarperamsetty <saikumar.peramsetty77@gmail.com>
2024-09-24 15:55:07 +0530

    list_part2  Membership Operators on list in Python?

---
+++ Python/basics/list/list_part2.py
@@ -0,0 +1,7 @@
+# Ex:27 Membership operators on list in Python?
+#  Membership operators = in, not in
+l = [10,30,20,90,40,50]
+print(90 in l)          # output = True
+print(180 in l)         # output = False
+print(60 not in l)      # output = True
+print(30 not in l)      # output = False
