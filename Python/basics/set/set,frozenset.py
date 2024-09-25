5faee3c907955185137bc7e4151a1ccc02f54b27
saikumarperamsetty <saikumar.peramsetty77@gmail.com>
2024-09-25 15:19:16 +0530

    set,frozenset: How many ways to create a set in Python?

---
+++ Python/basics/set/set,frozenset.py
@@ -0,0 +1,17 @@
+# Set{} Datatype:
+
+#Ex:1 How to create a set in Python?
+# by using set() function?
+s = set((1,2,3,4,5))
+print(s)
+print(type(s))
+
+# by using set() function How to convert ?
+s = set([1,2,3,4,5])
+print(s)
+print(type(s))
+
+# by using set{} datatype to create a set?
+s = {1,2,3,4,5}
+print(s)
+print(type(s))
