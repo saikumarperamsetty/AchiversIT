fdcd2007dd4ffd4b46f16bd9cc0968711921ff37
saikumarperamsetty <saikumar.peramsetty77@gmail.com>
2024-09-27 23:15:36 +0530

    Polymorphism with hasattr() method in Python?

--- Python/basics/oops/oops7.py
+++ Python/basics/oops/oops7.py
@@ -2,8 +2,26 @@
 # It refer to the ability or different classes to define methods that share the same name but have different implementation or behaviour.
 
 # Ex:1 Basic Example of Polymorphism in Python?
+# class Duck:
+#     def talk(self):
+#         print('Quack..Quack..')
+# class Cat:
+#     def talk(self):
+#         print('Meow..Meow..')
+# class Dog:
+#     def talk(self):
+#         print('Bow..Bow..')
+
+# def f1(obj):
+#     obj.talk()    
+# l = [Duck(), Cat(), Dog()]
+# for obj in l:
+#     f1(obj)
+
+
+# Ex:2 Polymorphism with hasattr() method in Python?
 class Duck:
-    def talk(self):
+    def bark(self):
         print('Quack..Quack..')
 class Cat:
     def talk(self):
@@ -13,7 +31,10 @@ class Dog:
         print('Bow..Bow..')
 
 def f1(obj):
-    obj.talk()
+    if hasattr(obj, 'talk'):
+        obj.talk()
+    elif hasattr(obj,'bark'):
+        obj.bark()  
 l = [Duck(), Cat(), Dog()]
 for obj in l:
     f1(obj)
