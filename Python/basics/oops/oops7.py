7e49da25cbaeb1115efd7b5a0fc48d11e8267a00
saikumarperamsetty <saikumar.peramsetty77@gmail.com>
2024-09-27 23:57:15 +0530

    update

--- Python/basics/oops/oops7.py
+++ Python/basics/oops/oops7.py
@@ -3,42 +3,42 @@
 # It refer to the ability or different classes to define methods that share the same name but have different implementation or behaviour.
 
 # Ex:1 Basic Example of Polymorphism in Python?
-# class Duck:
-#     def talk(self):
-#         print('Quack..Quack..')
-# class Cat:
-#     def talk(self):
-#         print('Meow..Meow..')
-# class Dog:
-#     def talk(self):
-#         print('Bow..Bow..')
+class Duck:
+    def talk(self):
+        print('Quack..Quack..')
+class Cat:
+    def talk(self):
+        print('Meow..Meow..')
+class Dog:
+    def talk(self):
+        print('Bow..Bow..')
 
-# def f1(obj):
-#     obj.talk()    
-# l = [Duck(), Cat(), Dog()]
-# for obj in l:
-#     f1(obj)
+def f1(obj):
+    obj.talk()    
+l = [Duck(), Cat(), Dog()]
+for obj in l:
+    f1(obj)
 
 
 # Ex:2 Polymorphism with hasattr() method in Python?
-# class Duck:
-#     def bark(self):
-#         print('Quack..Quack..')
-# class Cat:
-#     def talk(self):
-#         print('Meow..Meow..')
-# class Dog:
-#     def talk(self):
-#         print('Bow..Bow..')
+class Duck:
+    def bark(self):
+        print('Quack..Quack..')
+class Cat:
+    def talk(self):
+        print('Meow..Meow..')
+class Dog:
+    def talk(self):
+        print('Bow..Bow..')
 
-# def f1(obj):
-#     if hasattr(obj, 'talk'):
-#         obj.talk()
-#     elif hasattr(obj,'bark'):
-#         obj.bark()  
-# l = [Duck(), Cat(), Dog()]
-# for obj in l:
-#     f1(obj)
+def f1(obj):
+    if hasattr(obj, 'talk'):
+        obj.talk()
+    elif hasattr(obj,'bark'):
+        obj.bark()  
+l = [Duck(), Cat(), Dog()]
+for obj in l:
+    f1(obj)
 
 
 # Overloading:
@@ -49,17 +49,17 @@
 # 3. Constructor Overloading
 
 # 1. Operator Overloading
-# class Book:
-#     def __init__(self,pages):
-#         self.pages = pages
-#     def __add__(self,other):
-#         return self.pages + other.pages
-#     def __mul__(self,other):
-#         return self.pages * other.pages
-# book1 = Book(10)
-# book2 = Book(20)
-# print(book1+book2)
-# print(book1*book2)
+class Book:
+    def __init__(self,pages):
+        self.pages = pages
+    def __add__(self,other):
+        return self.pages + other.pages
+    def __mul__(self,other):
+        return self.pages * other.pages
+book1 = Book(10)
+book2 = Book(20)
+print(book1+book2)
+print(book1*book2)
 
 # 1.1 Operator Overloading with Two Diffrent Classes?
 class Employee:
