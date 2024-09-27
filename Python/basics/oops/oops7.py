189c849d61925bd64f82e58fd856f5019d8dbf42
saikumarperamsetty <saikumar.peramsetty77@gmail.com>
2024-09-27 23:40:02 +0530

    Operator Overloading in Polymorphism?

--- Python/basics/oops/oops7.py
+++ Python/basics/oops/oops7.py
@@ -1,4 +1,5 @@
-# Polymorphism: 
+# Polymorphism:
+# =============
 # It refer to the ability or different classes to define methods that share the same name but have different implementation or behaviour.
 
 # Ex:1 Basic Example of Polymorphism in Python?
@@ -20,21 +21,42 @@
 
 
 # Ex:2 Polymorphism with hasattr() method in Python?
-class Duck:
-    def bark(self):
-        print('Quack..Quack..')
-class Cat:
-    def talk(self):
-        print('Meow..Meow..')
-class Dog:
-    def talk(self):
-        print('Bow..Bow..')
+# class Duck:
+#     def bark(self):
+#         print('Quack..Quack..')
+# class Cat:
+#     def talk(self):
+#         print('Meow..Meow..')
+# class Dog:
+#     def talk(self):
+#         print('Bow..Bow..')
 
-def f1(obj):
-    if hasattr(obj, 'talk'):
-        obj.talk()
-    elif hasattr(obj,'bark'):
-        obj.bark()  
-l = [Duck(), Cat(), Dog()]
-for obj in l:
-    f1(obj)
+# def f1(obj):
+#     if hasattr(obj, 'talk'):
+#         obj.talk()
+#     elif hasattr(obj,'bark'):
+#         obj.bark()  
+# l = [Duck(), Cat(), Dog()]
+# for obj in l:
+#     f1(obj)
+
+
+# Overloading:
+# ============
+# Types Overloading:
+# 1. Operator Overloading
+# 2. Method Overloading
+# 3. Constructor Overloading
+
+# 1. Operator Overloading
+class Book:
+    def __init__(self,pages):
+        self.pages = pages
+    def __add__(self,other):
+        return self.pages + other.pages
+    def __mul__(self,other):
+        return self.pages * other.pages
+book1 = Book(10)
+book2 = Book(20)
+print(book1+book2)
+print(book1*book2)
