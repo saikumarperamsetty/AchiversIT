bbf5da9f571bbf88d43b35236ee90b53f2c259fc
saikumarperamsetty <saikumar.peramsetty77@gmail.com>
2024-09-28 13:37:07 +0530

    Method Overloading with Two Arguments?

--- Python/basics/oops/oops7.py
+++ Python/basics/oops/oops7.py
@@ -3,42 +3,42 @@
 # It refer to the ability or different classes to define methods that share the same name but have different implementation or behaviour.
 
 # Ex:1 Basic Example of Polymorphism in Python?
-class Duck:
-    def talk(self):
-        print('Quack..Quack..')
-class Cat:
-    def talk(self):
-        print('Meow..Meow..')
-class Dog:
-    def talk(self):
-        print('Bow..Bow..')
+# class Duck:
+#     def talk(self):
+#         print('Quack..Quack..')
+# class Cat:
+#     def talk(self):
+#         print('Meow..Meow..')
+# class Dog:
+#     def talk(self):
+#         print('Bow..Bow..')
 
-def f1(obj):
-    obj.talk()    
-l = [Duck(), Cat(), Dog()]
-for obj in l:
-    f1(obj)
+# def f1(obj):
+#     obj.talk()    
+# l = [Duck(), Cat(), Dog()]
+# for obj in l:
+#     f1(obj)
 
 
 # Ex:2 Polymorphism with hasattr() method in Python?
-class Duck:
-    def bark(self):
-        print('Quack..Quack..')
-class Cat:
-    def talk(self):
-        print('Meow..Meow..')
-class Dog:
-    def talk(self):
-        print('Bow..Bow..')
+# class Duck:
+#     def bark(self):
+#         print('Quack..Quack..')
+# class Cat:
+#     def talk(self):
+#         print('Meow..Meow..')
+# class Dog:
+#     def talk(self):
+#         print('Bow..Bow..')
 
-def f1(obj):
-    if hasattr(obj, 'talk'):
-        obj.talk()
-    elif hasattr(obj,'bark'):
-        obj.bark()  
-l = [Duck(), Cat(), Dog()]
-for obj in l:
-    f1(obj)
+# def f1(obj):
+#     if hasattr(obj, 'talk'):
+#         obj.talk()
+#     elif hasattr(obj,'bark'):
+#         obj.bark()  
+# l = [Duck(), Cat(), Dog()]
+# for obj in l:
+#     f1(obj)
 
 
 # Overloading:
@@ -49,28 +49,47 @@ for obj in l:
 # 3. Constructor Overloading
 
 # 1. Operator Overloading
-class Book:
-    def __init__(self,pages):
-        self.pages = pages
-    def __add__(self,other):
-        return self.pages + other.pages
-    def __mul__(self,other):
-        return self.pages * other.pages
-book1 = Book(10)
-book2 = Book(20)
-print(book1+book2)
-print(book1*book2)
+# =======================
+# class Book:
+#     def __init__(self,pages):
+#         self.pages = pages
+#     def __add__(self,other):
+#         return self.pages + other.pages
+#     def __mul__(self,other):
+#         return self.pages * other.pages
+# book1 = Book(10)
+# book2 = Book(20)
+# print(book1+book2)
+# print(book1*book2)
 
 # 1.1 Operator Overloading with Two Diffrent Classes?
-class Employee:
-    def __init__(self,name,salary):
-        self.name = name
-        self.salary = salary
-    def __mul__(self,other):
-        return self.salary * other.days
-class Timesheet:
-    def __init__(self,days):
-        self.days = days
-obj1 = Employee('Sai Kumar',1300)
-obj2 = Timesheet(27)
-print(obj1*obj2)
+# class Employee:
+#     def __init__(self,name,salary):
+#         self.name = name
+#         self.salary = salary
+#     def __mul__(self,other):
+#         return self.salary * other.days
+# class Timesheet:
+#     def __init__(self,days):
+#         self.days = days
+# obj1 = Employee('Sai Kumar',1300)
+# obj2 = Timesheet(27)
+# print(obj1*obj2)
+
+
+# 2. Method Overloading:
+# ----------------------
+# it is not supported in Python.
+# Ex:1
+class Test:
+    def sum(self, a=None,b=None,c=None):
+        if a != None and b != None and c != None:
+            print('The Sum:',a+b+c)
+        elif a != None and b != None:
+            print('The Sum:',a+b)
+        else:
+            print('Provide two or more arguments')
+obj = Test()
+obj.sum(10,20,30)
+obj.sum(20,30)
+obj.sum(10)
