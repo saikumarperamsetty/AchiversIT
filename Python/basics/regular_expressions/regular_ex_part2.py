977a7c6a45b0b45c38e1b7aa75fc2adc29d363aa
saikumarperamsetty <saikumar.peramsetty77@gmail.com>
2024-09-29 17:01:46 +0530

    purge() method in Regular Expression?

--- Python/basics/regular_expressions/regular_ex_part2.py
+++ Python/basics/regular_expressions/regular_ex_part2.py
@@ -2,6 +2,11 @@
 # ==========================
 
 # Ex:9 escape():
+# import re
+# regex_escape = re.escape('Hello? (Are you There?)')  # Here ?,(, ), space are special symbols, it replaces with escape symbols
+# print(regex_escape)     # output = Hello\?\ \(Are\ you\ There\?\)
+
+
+# Ex: 10 purge(): it will clear the Regular Expression cashe data
 import re
-regex_escape = re.escape('Hello? (Are you There?)')  # Here ?,(, ), space are special symbols, it replaces with escape symbols
-print(regex_escape)     # output = Hello\?\ \(Are\ you\ There\?\)
+print(re.purge())   # output = None
