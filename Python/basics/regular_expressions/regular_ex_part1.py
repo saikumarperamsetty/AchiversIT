91e16368be2b03ebf7cabfe6fbcc9288e1bd752f
saikumarperamsetty <saikumar.peramsetty77@gmail.com>
2024-09-29 16:29:36 +0530

    Python Regular Expressions Part-1?  all methods Completed

--- Python/basics/regular_expressions/regular_ex_part1.py
+++ Python/basics/regular_expressions/regular_ex_part1.py
@@ -12,14 +12,14 @@
 # iii. group(): return the group index
 
 # Ex:1 Basic Example to Pattern Find in Given String By Using Regular Expression?
-# import re
-# count = 0
-# # pattern = re.compile('ab')
-# matcher = re.finditer('ab','abaaababa')
-# for match in matcher:
-#     count += 1
-#     print(match.start(),'....',match.end(),'....',match.group())    # output = 0 .... 2 .... ab, 4 .... 6 .... ab, 6 .... 8 .... ab
-# print('The number of occurences of ab:',count)                      # output = 3
+import re
+count = 0
+# pattern = re.compile('ab')
+matcher = re.finditer('ab','abaaababa')
+for match in matcher:
+    count += 1
+    print(match.start(),'....',match.end(),'....',match.group())    # output = 0 .... 2 .... ab, 4 .... 6 .... ab, 6 .... 8 .... ab
+print('The number of occurences of ab:',count)                      # output = 3
 
 
 # Character Classes in Regular Expression:
@@ -34,19 +34,19 @@
 # [^a-zA-Z0-9] = except a-zA-Z0-9
 
 # Ex:2 Pattern Find in Given String By Using Character Classes in Regular Expression?
-# import re
-# x = '[abc]'
-# x = '[^abc]'
-# x = '[a-z]'
-# x = '[^a-z]'
-# x = '[A-Z]'
-# x = '[^A-Z]'
-# x = '[a-zA-Z0-9]'
-# x = '[^a-zA-Z0-9]'
-# pattern= re.compile(x)
-# matcher = pattern.finditer('a7N@k9bTd2R8$')
-# for match in matcher:
-#     print(match.start(),'....',match.group())
+import re
+x = '[abc]'
+x = '[^abc]'
+x = '[a-z]'
+x = '[^a-z]'
+x = '[A-Z]'
+x = '[^A-Z]'
+x = '[a-zA-Z0-9]'
+x = '[^a-zA-Z0-9]'
+pattern= re.compile(x)
+matcher = pattern.finditer('a7N@k9bTd2R8$')
+for match in matcher:
+    print(match.start(),'....',match.group())
 
 
 # Predefined Character Classes in Regular Expression:
@@ -61,19 +61,19 @@
 # . - include dot(.) all other chars
 
 # Ex:3 Pattern Find By Using Predefined Character Classes in Regular Expression?
-# import re
-# x = '\s'    #only space
-# x = '\S'    #Except space, any other char
-# x = '\d'    #only digits
-# x = '\D'    #Except digits, any other char
-# x = '\w'    #it represents the Alpha_numeric, Except Special Chars
-# x = '\W'    # Except Alpha_numeric, only Special Chars
-# x = '\.'    #only dot(.)
-# x = '.'     #include dot(.), all other chars
-# pattern= re.compile(x)
-# matcher = pattern.finditer('a7N @k.9bT d2.R 8$')
-# for match in matcher:
-#     print(match.start(),'....',match.group())
+import re
+x = '\s'    #only space
+x = '\S'    #Except space, any other char
+x = '\d'    #only digits
+x = '\D'    #Except digits, any other char
+x = '\w'    #it represents the Alpha_numeric, Except Special Chars
+x = '\W'    # Except Alpha_numeric, only Special Chars
+x = '\.'    #only dot(.)
+x = '.'     #include dot(.), all other chars
+pattern= re.compile(x)
+matcher = pattern.finditer('a7N @k.9bT d2.R 8$')
+for match in matcher:
+    print(match.start(),'....',match.group())
 
 
 # Quantifiers in Regular Expression:
@@ -87,81 +87,81 @@
 
 
 # Ex:4 Pattern Find By Using Quantifiers in Regular Expression?
-# import re
-# x = 'a'    # Exactly one time matching a string, it will return
-# x = 'a+'    # Min 1 time of a - Max any no of time of a, wherever a is came in String that will return
-# x = 'a*'    # Min 0 time of a - Max any no of time of a, wherever a is came in String that will return and include other chars(its hiding) also
-# x = 'a?'    # Min 0 time of a - Max 1 time of a, wherever a is came in String that will return
-# x = 'a{2}'    # Exactly m time( m- means number we want to mention), wherever a is came in String that will only return
-# x = 'a{2,5}'    # Exactly m,n time( m,n- means number we want to mention), wherever a is came in String that will only return
-# pattern= re.compile(x)
-# matcher = pattern.finditer('abaabaaab')
-# for match in matcher:
-#     print(match.start(),'....',match.group())
+import re
+x = 'a'    # Exactly one time matching a string, it will return
+x = 'a+'    # Min 1 time of a - Max any no of time of a, wherever a is came in String that will return
+x = 'a*'    # Min 0 time of a - Max any no of time of a, wherever a is came in String that will return and include other chars(its hiding) also
+x = 'a?'    # Min 0 time of a - Max 1 time of a, wherever a is came in String that will return
+x = 'a{2}'    # Exactly m time( m- means number we want to mention), wherever a is came in String that will only return
+x = 'a{2,5}'    # Exactly m,n time( m,n- means number we want to mention), wherever a is came in String that will only return
+pattern= re.compile(x)
+matcher = pattern.finditer('abaabaaab')
+for match in matcher:
+    print(match.start(),'....',match.group())
 
 
 # Functons in re module:
 # ======================
 # 1. match():
-# import re
-# s = input('Enter Pattern to Check: ')
-# m = re.match(s,'abcfghqdnd')              #Exactly Not match the Given String
-# if m != None:
-#     print('Match String is available in Given string')
-#     print('Start index:',m.start(),'end index:',m.end())
-# else:
-#     print('Match String is Not available in Given string')
+import re
+s = input('Enter Pattern to Check: ')
+m = re.match(s,'abcfghqdnd')              #Exactly Not match the Given String
+if m != None:
+    print('Match String is available in Given string')
+    print('Start index:',m.start(),'end index:',m.end())
+else:
+    print('Match String is Not available in Given string')
 
 
 # 2. fullmatch():
-# import re
-# s = input('Enter Pattern to Check: ')
-# m = re.fullmatch(s,'abcfghqdnd')        #Exactly Match the Given String
-# if m != None:
-#     print('Match String is available in Given string')
-#     print('Start index:',m.start(),'end index:',m.end())
-# else:
-#     print('Match String is Not available in Given string')
+import re
+s = input('Enter Pattern to Check: ')
+m = re.fullmatch(s,'abcfghqdnd')        #Exactly Match the Given String
+if m != None:
+    print('Match String is available in Given string')
+    print('Start index:',m.start(),'end index:',m.end())
+else:
+    print('Match String is Not available in Given string')
 
 
 # 3. finditer():
-# import re
-# itr = re.finditer('[a-z]','a0b2*be@kd%jo$')       # Wherever the Pattern matches only that elements will return.
-# for match in itr:
-#     print('index present at:',match.start(),'---',match.group())
+import re
+itr = re.finditer('[a-z]','a0b2*be@kd%jo$')       # Wherever the Pattern matches only that elements will return.
+for match in itr:
+    print('index present at:',match.start(),'---',match.group())
 
 
 # 4. sub():
 # syntax = [regex,replacement,targetstring]
-# import re
-# substr = re.sub('[a-z]','$','a0b2*be@kd%jo$')       # Wherever the applied symbol($) Pattern matches only that elements will changed with new string.
-# print(substr)                                 # output = $0$2*$$@$$%$$$ (here Where a-z elements is there that only will Change)
+import re
+substr = re.sub('[a-z]','$','a0b2*be@kd%jo$')       # Wherever the applied symbol($) Pattern matches only that elements will changed with new string.
+print(substr)                                 # output = $0$2*$$@$$%$$$ (here Where a-z elements is there that only will Change)
 
 
 # 5. subn():
 # here 0 index represents the result String
 # here 1 index represents the Occurences
-# import re
-# substrn = re.subn('[a-z]','$','a0b2*be@kd%jo$')       # Wherever the applied symbol($) Pattern matches only that elements will changed with new string.
-# print(substrn)                                 # output = ($0$2*$$@$$%$$$,8) (here Where a-z elements is there that only will Change), and return How many times that symbol is occured
-# print('The Result String:',substrn[0])      # output = $0$2*$$@$$%$$$                          
-# print('The no of Occurences: ',substrn[1])  # 8
+import re
+substrn = re.subn('[a-z]','$','a0b2*be@kd%jo$')       # Wherever the applied symbol($) Pattern matches only that elements will changed with new string.
+print(substrn)                                 # output = ($0$2*$$@$$%$$$,8) (here Where a-z elements is there that only will Change), and return How many times that symbol is occured
+print('The Result String:',substrn[0])      # output = $0$2*$$@$$%$$$                          
+print('The no of Occurences: ',substrn[1])  # 8
 
 
 # 6. split():
-# import re
-# regex_split = re.split('@','Apple,Watermelon,Orange,Cocumber,Tomato')   # here only that ',' only seperated
-# regex_split = re.split('@','Apple@Watermelon@Orange@Cocumber@Tomato')    # here only that '@' only seperated
-# print(regex_split)      # output = ['Apple', 'Watermelon', 'Orange', 'Cocumber', 'Tomato'] it will return ouput in the form of list as comma seperated.
+import re
+regex_split = re.split('@','Apple,Watermelon,Orange,Cocumber,Tomato')   # here only that ',' only seperated
+regex_split = re.split('@','Apple@Watermelon@Orange@Cocumber@Tomato')    # here only that '@' only seperated
+print(regex_split)      # output = ['Apple', 'Watermelon', 'Orange', 'Cocumber', 'Tomato'] it will return ouput in the form of list as comma seperated.
 
 
 # 7. ^symbol(): starts with
-# import re
-# regex_symbol = re.search('^Laerning','Learning Python is Very Easy!')       # output = False
-# if regex_symbol != None:
-#     print('True')
-# else:
-#     print('False')
+import re
+regex_symbol = re.search('^Laerning','Learning Python is Very Easy!')       # output = False
+if regex_symbol != None:
+    print('True')
+else:
+    print('False')
 
 
 # 8. $symbol(): ends with
