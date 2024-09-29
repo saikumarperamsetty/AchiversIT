e1d1ee019ff2f0c3e63ae3a9d47ead16fff6cef9
saikumarperamsetty <saikumar.peramsetty77@gmail.com>
2024-09-29 17:24:22 +0530

    Grouping() and Capturing() in Regular Expression?

--- Python/basics/regular_expressions/regular_ex_part2.py
+++ Python/basics/regular_expressions/regular_ex_part2.py
@@ -8,5 +8,18 @@
 
 
 # Ex: 10 purge(): it will clear the Regular Expression cashe data
+# import re
+# print(re.purge())   # output = None
+
+
+# Grouping() and Capturing() in Regular Expression:
 import re
-print(re.purge())   # output = None
+text = 'Sai: 123-456-3391'
+pattern = r'(\w+): (\d{3})-(\d{3})-(\d{4})'
+match = re.search(pattern,text)
+if match:
+    print('Group 1:',match.group(1))    # output = Sai:
+    print('Group 2:',match.group(2))    # output = 123
+    print('Group 3:',match.group(3))    # output = 456
+    print('Group 3:',match.group(4))    # output = 3391
+    print('Group 0:',match.group(0))    # output = Sai: 123-456-3391
