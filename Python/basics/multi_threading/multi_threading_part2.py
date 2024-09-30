6f1dd1c6b0a39cc7b161412f0b514f0ce7375879
saikumarperamsetty <saikumar.peramsetty77@gmail.com>
2024-09-30 15:23:27 +0530

    How to set Daemon Nature for current Thread or any other Thread?

--- Python/basics/multi_threading/multi_threading_part2.py
+++ Python/basics/multi_threading/multi_threading_part2.py
@@ -1,8 +1,25 @@
 # Multi Threading part-2:
-# What is Daemon Treads?
-# Ans: Thre treads which are running in the background is called Deamon Thread.
+# ======================
+--> Main Thread is always non Daemon Thread only.
+
+# What is Daemon Treads:
+# ======================
+Ans: Thre treads which are running in the background is called Deamon Thread.
+# ---
 
 # Ex:1 How to check current Thread is Daemon Thread or not?
+# =========================================================
 import threading
-print(threading.current_thread().isDaemon())    # output = False
-print(threading.current_thread().daemon)        # output = False
+# Approach-1
+print(threading.current_thread().isDaemon())    # output = False    (it will Check the Current Thread is Daemon or Not)
+
+# Approach-2
+print(threading.current_thread().daemon)        # output = False    (it will Check the Current Thread is Daemon or Not)
+
+
+# Ex:2 How to set Daemon Nature for current Thread or any other Thread?
+# =====================================================================
+from threading import Thread
+t = Thread(target=lambda:None)
+t.setDaemon(True)                   # it will set thread behaviour is Daemon now
+print(t.isDaemon())                 # output = True
