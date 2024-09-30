4e4f543cd1cb2f9bd1ed91f4957a856356dbb9ac
saikumarperamsetty <saikumar.peramsetty77@gmail.com>
2024-09-30 15:40:19 +0530

    How to check Daemon Nature and Non Daemon Nature of current Executing Thread or any other Thread?

--- Python/basics/multi_threading/multi_threading_part2.py
+++ Python/basics/multi_threading/multi_threading_part2.py
@@ -1,25 +1,44 @@
 # Multi Threading part-2:
 # ======================
---> Main Thread is always non Daemon Thread only.
+# --> Main Thread is always non Daemon Thread only.
 
 # What is Daemon Treads:
 # ======================
-Ans: Thre treads which are running in the background is called Deamon Thread.
+# Ans: Thre treads which are running in the background is called Deamon Thread.
 # ---
 
 # Ex:1 How to check current Thread is Daemon Thread or not?
 # =========================================================
-import threading
+# import threading
 # Approach-1
-print(threading.current_thread().isDaemon())    # output = False    (it will Check the Current Thread is Daemon or Not)
+# print(threading.current_thread().isDaemon())    # output = False    (it will Check the Current Thread is Daemon or Not)
 
 # Approach-2
-print(threading.current_thread().daemon)        # output = False    (it will Check the Current Thread is Daemon or Not)
+# print(threading.current_thread().daemon)        # output = False    (it will Check the Current Thread is Daemon or Not)
 
 
 # Ex:2 How to set Daemon Nature for current Thread or any other Thread?
 # =====================================================================
-from threading import Thread
-t = Thread(target=lambda:None)
-t.setDaemon(True)                   # it will set thread behaviour is Daemon now
-print(t.isDaemon())                 # output = True
+# from threading import Thread
+# t = Thread(target=lambda:None)
+# t.setDaemon(True)                   # it will set thread behaviour is Daemon now
+# print(t.isDaemon())                 # output = True
+
+
+# Ex:3 How to check Daemon Nature and Non Daemon Nature of current Executing Thread or any other Thread?
+# ======================================================================================================
+from threading import *
+import time
+def check_daemon_behaviour():
+    for i in range(1,6):
+        print('child thread: ',i)
+        time.sleep(1)
+t = Thread(target=check_daemon_behaviour)
+t.setDaemon(True)
+
+t.start()
+time.sleep(5)
+
+for j in range(1,6):
+    print('main thread: ',j)
+print('end of main thread')
