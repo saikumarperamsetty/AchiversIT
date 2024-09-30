cfd7840ad2c9ec521f6591ed77518583424e44ea
saikumarperamsetty <saikumar.peramsetty77@gmail.com>
2024-09-30 22:48:52 +0530

    What is queue module methods? like Queue(), LifoQueue(), PriorityQueue()?

--- Python/basics/multi_threading/multi_threading_part3.py
+++ Python/basics/multi_threading/multi_threading_part3.py
@@ -55,24 +55,66 @@
 
 # Ex: Queue() method in Inter Thread Communication?
 # =================================================
-import threading
-import time
-import random
+# import threading
+# import time
+# import random
+# import queue
+# q = queue.Queue()
+# def producer(q):
+#     while True:
+#         item = random.randint(1,101)
+#         print('Producer producing the item',item)
+#         q.put(item)
+#         print('Producer giving Notification')
+#         time.sleep(3)
+# def consumer(q):
+#     while True:
+#         print('Consumer waiting for Updation')
+#         print('Consumer consumed the item',q.get())
+#         time.sleep(3)
+# t1 = threading.Thread(target=producer,args=(q,))
+# t2 = threading.Thread(target=consumer,args=(q,))
+# t1.start()
+# t2.start()
+
+
+queue module methods:
+=====================
+1. LIFO(LastInFirstOut)
+2. FIFO(FirstInLastOut)
+3. Priority()
+
+1. LIFO(LastInFirstOut)
+=======================
+1.1 Queue():
+===========
 import queue
 q = queue.Queue()
-def producer(q):
-    while True:
-        item = random.randint(1,101)
-        print('Producer producing the item',item)
-        q.put(item)
-        print('Producer giving Notification')
-        time.sleep(3)
-def consumer(q):
-    while True:
-        print('Consumer waiting for Updation')
-        print('Consumer consumed the item',q.get())
-        time.sleep(3)
-t1 = threading.Thread(target=producer,args=(q,))
-t2 = threading.Thread(target=consumer,args=(q,))
-t1.start()
-t2.start()      
+q.put(15)
+q.put(5)
+q.put(20)
+q.put(10)
+while not q.empty():
+    print(q.get())
+
+1.2 LifoQueue():
+===============
+import queue
+q = queue.LifoQueue()
+q.put(15)
+q.put(5)
+q.put(20)
+q.put(10)
+while not q.empty():
+    print(q.get())
+
+1.2 PriorityQueue():
+===================
+import queue
+q = queue.PriorityQueue()
+q.put((15,'SAI'))
+q.put((5,'NTR'))
+q.put((20,'SURAJ'))
+q.put((10,'PYTHON'))
+while not q.empty():
+    print(q.get())
