4872cce683169e06a36b9513072958251f830c0b
saikumarperamsetty <saikumar.peramsetty77@gmail.com>
2024-10-02 18:54:35 +0530

    CRUD Operations in PDBC?

--- Python/basics/PDBC/pdbc.py
+++ Python/basics/PDBC/pdbc.py
@@ -1,30 +1,30 @@
 # Database Connection in Python:
 # =============================
---> It means of establishing communication between a software component and a database to enable interaction.
+# --> It means of establishing communication between a software component and a database to enable interaction.
 
 # Two types of storage system:
 # ============================
 # 1. temporary: 
 # ------------
---> list, tuple, set and dictonary.
+# --> list, tuple, set and dictonary.
 
 # 2. persistent: 
 # ------------
---> file, database, data warehouses, Big data technologies etc....
+# --> file, database, data warehouses, Big data technologies etc....
 
 # Database key Features:
 # =====================
-1. Data Management.
-2. Data Integrity.
-3. Data Retrieval:
-4. Concurrency control:
-5. Security:
-6. Backup and Recovery:
-7. Support for application:
-8. Analysis and Reporting:
-9. Reduced Data redundancy:
-10. Versatile:
-11. Scalable:
+# 1. Data Management.
+# 2. Data Integrity.
+# 3. Data Retrieval:
+# 4. Concurrency control:
+# 5. Security:
+# 6. Backup and Recovery:
+# 7. Support for application:
+# 8. Analysis and Reporting:
+# 9. Reduced Data redundancy:
+# 10. Versatile:
+# 11. Scalable:
 
 
 # Python Database Connection:
@@ -34,17 +34,17 @@
 
 # Standard Steps for any Database:
 # ================================
-For MYSQL Databse:
-------------------
-Step 1: myssql.connector.connect()
-Step 2: conn.cursor()
-Step 3: cursor.execute()
-Step 4: conn.commit()
-Step 5: cursor.close() and conn.close()
+# For MYSQL Databse:
+# ------------------
+# Step 1: myssql.connector.connect()
+# Step 2: conn.cursor()
+# Step 3: cursor.execute()
+# Step 4: conn.commit()
+# Step 5: cursor.close() and conn.close()
 
 # sqlite3 Database:
 # =================
-Ex: How to Write Queries in sqlite3 Database?
+# Ex: How to Write Queries in sqlite3 Database?
 # =============================================
 import sqlite3
 connection = sqlite3.connect('sqlite3.db')
@@ -73,13 +73,13 @@ connection.close()
 
 # Ex: How to Give connection for Python - MySQL Database in Python?
 # =================================================================
-First istall MySQL environment on your Python.
-pip install mysql-connector-python
+# First istall MySQL environment on your Python.
+# pip install mysql-connector-python
 
 
 # Python - PostgreSQL Database connection:
 # =======================================
-pip install psycopg2
+# pip install psycopg2
 
 import mysql.connector
 from config import DB_CONFIG
