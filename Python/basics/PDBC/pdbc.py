67f2d4cbe13695cfbb9694994f56bdfda22011c5
saikumarperamsetty <saikumar.peramsetty77@gmail.com>
2024-10-02 11:53:36 +0530

    PDBC key features

---
+++ Python/basics/PDBC/pdbc.py
@@ -0,0 +1,27 @@
+# Database Connection in Python:
+# =============================
+--> It means of establishing communication between a software component and a database to enable interaction.
+
+# Two types of storage system:
+# ============================
+1. temporary: 
+# ------------
+--> list, tuple, set and dictonary.
+
+2. persistent: 
+# ------------
+--> file, database, data warehouses, Big data technologies etc....
+
+# Database key Features:
+# =====================
+1. Data Management.
+2. Data Integrity.
+3. Data Retrieval:
+4. Concurrency control:
+5. Security:
+6. Backup and Recovery:
+7. Support for application:
+8. Analysis and Reporting:
+9. Reduced Data redundancy:
+10. Versatile:
+11. Scalable:
